e8b1a024166b4c10e527b2304ae41352f1d5d530
JohnTo02 <to01@ads.uni-passau.de>
2023-12-24 02:30:06 +0100

    * added datenanfrage-stellen * added daten-freigabe * added offene-pvanlagen

--- backend/app/types.py
+++ backend/app/types.py
@@ -8,28 +8,23 @@ class Rolle(enum.Enum):
     Netzbetreiber = 'Netzbetreiber'
     Admin = 'Admin'
 
+
 class Isolierungsqualitaet(enum.Enum):
     hoch = 'hoch'
     mittel = 'mittel'
     niedrig = 'niedrig'
 
-class AusrichtungDach(enum.Enum):
-    Nord = 'Nord'
-    Nordost = 'Nordost'
-    Ost = 'Ost'
-    Suedost = 'Suedost'
-    Sued = 'Sued'
-    Suedwest = 'Suedwest'
-    West = 'West'
-    Nordwest = 'Nordwest'
 
 class Rechnungsart(enum.Enum):
     Netzbetreiber_Rechnung = 'Netzbetreiber_Rechnung'
     Energieberater_Rechnung = 'Energieberater_Rechnung'
     Solarteur_Rechnung = 'Solarteur_Rechnung'
 
+
 class ProzessStatus(enum.Enum):
     AnfrageGestellt = 'AnfrageGestellt'
+    DatenAngefordert = 'DatenAngefordert'
+    DatenFreigegeben = 'DatenFreigegeben'
     AngebotGemacht = 'AngebotGemacht'
     AngebotAngenommen = 'AngebotAngenommen'
     PlanErstellt = 'PlanErstellt'

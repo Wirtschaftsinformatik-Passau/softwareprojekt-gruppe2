f782a67257fb810d6139ab6c52a0a7bc7f935b43
SimonGithub00 <simon.anthofer@web.de>
2024-01-18 17:23:21 +0100

    updateuserUeberarbeitetV2

--- backend/app/models.py
+++ backend/app/models.py
@@ -228,7 +228,7 @@ class Energieeffizienzmassnahmen(Base):
 class Kündigungsanfrage(Base):
     __tablename__ = 'kündigungsanfrage' if settings.OS == 'Linux' else "Kündigungsanfrage"
     anfrage_id = Column(Integer, primary_key=True)
-    vertrag_id = Column(Integer, ForeignKey('vertrag.vertrag_id'))
+    vertrag_id = Column(Integer, ForeignKey('vertrag.vertrag_id' if settings.OS == 'Linux' else 'Vertrag.vertrag_id'))
     bestätigt = Column(Boolean, default=False)
     
     vertrag = relationship("Vertrag", back_populates="kündigungsanfrage")

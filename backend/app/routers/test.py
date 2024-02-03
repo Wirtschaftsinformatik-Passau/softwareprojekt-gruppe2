f3126ddcb27cc3e5a38b19e3b158b6499f0fcaea
SimonGithub00 <simon.anthofer@web.de>
2024-02-03 19:18:17 +0100

    solarteur und test docs

--- backend/app/routers/test.py
+++ backend/app/routers/test.py
@@ -2,6 +2,26 @@ from geopy.geocoders import Nominatim
 from geopy.exc import GeocoderTimedOut
 
 def geocode_address(street, number, postal_code, city):
+    """
+    Geokodiert eine Adresse basierend auf Straße, Hausnummer, PLZ und Stadt.
+
+    Args:
+        street (str): Der Straßenname.
+        number (str): Die Hausnummer.
+        postal_code (str): Die Postleitzahl.
+        city (str): Die Stadt.
+
+    Returns:
+        Tuple[float, float]: Ein Tupel mit Latitude und Longitude, wenn die Adresse gefunden wurde,
+                             andernfalls (None, None).
+
+    Example:
+        latitude, longitude = geocode_address("Höllgasse", "22", "94032", "Passau")
+        if latitude and longitude:
+            print(f"Koordinaten: Latitude = {latitude}, Longitude = {longitude}")
+        else:
+            print("Adresse nicht gefunden oder ein Fehler ist aufgetreten")
+    """
     geolocator = Nominatim(user_agent="ToseBackend")
     query = {
         'street': f'{number} {street}',
